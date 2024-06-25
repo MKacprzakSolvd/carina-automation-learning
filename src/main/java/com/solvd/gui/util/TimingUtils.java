0b52a0c86585c0ad6a300740a7ad02e9802859f1
MKacprzakSolvd <170347259+MKacprzakSolvd@users.noreply.github.com>
2024-06-25 23:22:23 +0200

    Changed timeMethod to accept Runnable instead of Supplier

--- src/main/java/com/solvd/gui/util/TimingUtils.java
+++ src/main/java/com/solvd/gui/util/TimingUtils.java
@@ -2,12 +2,10 @@ package com.solvd.gui.util;
 
 import org.apache.logging.log4j.Logger;
 
-import java.util.function.Supplier;
-
 public class TimingUtils {
-    public static void timeMethod(Supplier<?> method, Logger logger) {
+    public static void timeMethod(Runnable method, Logger logger) {
         long start = System.nanoTime();
-        method.get();
+        method.run();
         long end = System.nanoTime();
         double durationSeconds = ((double) (end - start)) / 1_000_000_000d;
         logger.info("Execution took %.6f seconds".formatted(durationSeconds));
